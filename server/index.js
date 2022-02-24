9a0d528f8f8224eb2579b38b50b6f2e3b7bd85cf
Mike Davis <mike.davis@psyvad.com>
2022-02-24 12:39:53 -0700

    added json parser

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
