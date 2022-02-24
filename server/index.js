f7be2c26755ad4844bd09a72dcb76689ab30e6fb
Mike Davis <mike.davis@psyvad.com>
2022-02-24 11:51:04 -0700

    change port

--- server/index.js
+++ server/index.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4545
 
-app.listen(4545, () => console.log(`Listening on port ${port}.`))
+app.listen(port, () => console.log(`Listening on port ${port}.`))
